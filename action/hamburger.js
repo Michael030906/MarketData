90ff4ad5174bbb58b5de05dbca8f5a393b0fdb6e
Tyler <tyboyer16@gmail.com>
2024-12-05 23:16:46 -0600

    added Hamburger for mobile

---
+++ action/hamburger.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("headerContainer");
+    if (x.className === "header") {
+      x.className += " responsive";
+    } else {
+      x.className = "header";
+    }
+  }
+  
